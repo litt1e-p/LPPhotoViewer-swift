1e932e991956955387df1efea22f3e7b1983bf3e
litt1e-p <free9fw@gmail.com>
2016-05-02 11:38:37 +0800

    generic delegate protocol for custom modal

---
+++ LPPhotoViewer/LPPhotoViewer+TransitionDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  LPPhotoViewer+TransitionDelegate.swift
+//  LPPhotoViewer
+//
+//  Created by litt1e-p on 16/4/28.
+//  Copyright © 2016年 litt1e-p. All rights reserved.
+//
+
+import UIKit
+
+extension LPPhotoViewer: UIViewControllerTransitioningDelegate
+{
+    func transitionMethod(type: LPPhotoViewTransitionType) -> UIViewControllerAnimatedTransitioning? {
+        if customTransition && customTransitionDelegate != nil {
+            return customTransitionDelegate?.genericTrantion(type)
+        } else {
+            return LPPhotoViewTransition(transitionType: type)
+        }
+    }
+    
+    func animationControllerForPresentedController(presented: UIViewController, presentingController presenting: UIViewController, sourceController source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return transitionMethod(.Present)
+    }
+    
+    func animationControllerForDismissedController(dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return transitionMethod(.Dismiss)
+    }
+}
